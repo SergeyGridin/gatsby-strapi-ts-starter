fce5df6778d1969e3e0f80b55e38dac04f4aec46
Sergey Gridin <gridin21@gmail.com>
2022-03-26 18:16:25 +0300

    refactor to include backend code

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'db8bbb5033ec40bf27cafe14fe398b27'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '97f49600-58b7-4f4a-baf2-7da0b5e7f536'
+};
